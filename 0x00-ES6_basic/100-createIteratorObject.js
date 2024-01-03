5ef7145e3f52d5e3818748b64d37da29a5b2f46a
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-03 13:27:20 +0300

    iterator through objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+    const allEmployees = [];
+    for (const key of Object.keys(report.allEmployees)) {
+        allEmployees.push(...report.allEmployees[key]);
+    }
+    
+    return allEmployees;
+}
