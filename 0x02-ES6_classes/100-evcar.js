db71d5fdea82fb1cf75d8122546602204194fc8f
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-04 16:12:04 +0300

    implement class that extends another class

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,17 +1,22 @@
 import Car from './10-car';
 
 class EVCar extends Car {
-  constructor(brand, motor, color, range) {
+  constructor(
+    brand = '',
+    motor = '',
+    color = '',
+    range = '',
+  ) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  cloneCar() {
-    return new this.constructor();
+  static get [Symbol.species]() {
+    return Car;
   }
 
-  static get [Symbol.species]() {
-    return this.prototype.constructor;
+  cloneCar() {
+    return new (this.constructor[Symbol.species])();
   }
 }
 export default EVCar;
