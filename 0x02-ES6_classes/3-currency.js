1d56940c655042c669562b33adac0335b1cdffef
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-04 12:59:02 +0300

    methods, static & computed

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+  /* use of get and set */
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string' && !(code instanceof String)) {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string' && !(name instanceof String)) {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+export default Currency;
