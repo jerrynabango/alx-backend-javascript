337a7083b634642b3a6e9c94f17cff7d3835dfc5
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-27 20:21:01 +0300

    organize complex server

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,14 +3,14 @@ const readDatabase = require('../utils');
 class StudentsController {
   static getAllStudents(request, response) {
     readDatabase(process.argv[2].toString()).then((students) => {
-      const studentList = []; // Changed variable name from output to studentList
-      studentList.push('This is the list of our students');
+      const output = [];
+      output.push('This is the list of our students');
       const keys = Object.keys(students);
       keys.sort();
-      for (let complex = 0; complex < keys.length; complex += 1) {
-        studentList.push(`Number of students in ${keys[i]}: ${students[keys[complex]].length}. List: ${students[keys[i]].join(', ')}`);
+      for (let i = 0; i < keys.length; i += 1) {
+        output.push(`Number of students in ${keys[i]}: ${students[keys[i]].length}. List: ${students[keys[i]].join(', ')}`);
       }
-      response.status(200).send(studentList.join('\n'));
+      response.status(200).send(output.join('\n'));
     }).catch(() => {
       response.status(500).send('Cannot load the database');
     });
