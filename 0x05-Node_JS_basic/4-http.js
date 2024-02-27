36d95278043f77046409e1a29e89538b95319fac
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-27 19:17:16 +0300

    small http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,11 +3,9 @@ const hostname = "127.0.0.1";
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    if (req.url === '/') {
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/plain");
         res.end("Hello Holberton School!");
-    }
 });
 
 app.listen(port, hostname, () => {
