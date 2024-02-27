e51175c6dc9c8cc3ba511328215ee8532684133f
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-27 17:33:27 +0300

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,28 +3,29 @@ const fs = require("fs");
 function countStudents(filepath) {
   let content;
   try {
-    content = fs.readFileSync(filepath, { encoding: 'utf8', flag: "r" });
+    content = fs.readFileSync(filepath, { encoding: "utf8", flag: "r" });
   } catch (err) {
     throw new Error("Cannot load the database");
   }
 
   const records = content.split('\n');
-  const compSci = [];
-  const softWare = [];
+  const computerScienceStudents = [];
+  const softwareEngineeringStudents = [];
 
   records.forEach((record) => {
     const field = record.split(",");
     if (field !== [] && field !== null) {
       if (field[3] === "CS") {
-        compSci.push(field[0]);
+        computerScienceStudents.push(field[0]);
       } else if (field[3] === "SWE") {
-        softWare.push(field[0]);
+        softwareEngineeringStudents.push(field[0]);
       }
     }
   });
-  console.log(`Number of students: ${compSci.length + softWare.length}`);
-  console.log(`Number of students in CS: ${compSci.length}. List: ${compSci.join(", ")}`);
-  console.log(`Number of students in SWE: ${softWare.length}. List: ${softWare.join(", ")}`);
+
+  console.log(`Number of students: ${computerScienceStudents.length + softwareEngineeringStudents.length}`);
+  console.log(`Number of students in CS: ${computerScienceStudents.length}. List: ${computerScienceStudents.join(", ")}`);
+  console.log(`Number of students in SWE: ${softwareEngineeringStudents.length}. List: ${softwareEngineeringStudents.join(", ")}`);
 }
 
 module.exports = countStudents;
