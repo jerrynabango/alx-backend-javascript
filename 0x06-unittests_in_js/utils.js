88f19a5ec68cdcc3593aad046a3d2172097c463c
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-29 18:01:51 +0300

    use of spies

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,25 +1,19 @@
-class Utils {
-    static calculateNumber(type, a, b) {
-	const an = Math.round(a);
-	const bn = Math.round(b);
-	let c = 0;
-	switch (type) {
-	case 'SUM':
-            c = an + bn;
-            break;
-	case 'SUBTRACT':
-            c = an - bn;
-            break;
-	case 'DIVIDE':
-	    if (bn === 0) {
-		c = "Error";
-	    } else {
-		c = an / bn;
-	    }
-	    break;
-	}
-	return c;
-    }
-}
-
-module.exports = Utils;
+const Utils = {
+    calculateNumber(type, a, b) {
+      let result;
+      if (type === 'SUM') {
+        result = Math.round(a) + Math.round(b);
+      } else if (type === 'SUBTRACT') {
+        result = Math.round(a) - Math.round(b);
+      } else if (type === 'DIVIDE') {
+        if (Math.abs(Math.round(b)) === 0) {
+          result = 'Error';
+        } else {
+          result = Math.round(a) / Math.round(b);
+        }
+      }
+      return result;
+    },
+  };
+  
+  module.exports = Utils;
