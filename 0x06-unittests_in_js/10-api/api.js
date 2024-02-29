942148194d009dc15370d503dedb77a225433f47
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-29 22:56:50 +0300

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,38 +1,35 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const port = 7865;
 
-app.get("/", (req, resp) => {
-  resp.send("Welcome to the payment system");
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Welcome to the payment system');
 });
 
-app.get("/cart/:id([0-9]+)", (req, resp) => {
-    resp.send(`Payment methods for cart ${req.params.id}`);
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  const { id } = req.params;
+  res.send(`Payment methods for cart ${id}`);
 });
 
-app.get("/available_payments", (req, resp) => {
-    resp.set("Content-Type", "application/json");
-    const payMethods = {
-	payment_methods: {
-          credit_cards: true,
-          paypal: false
-	}
-    }
-    resp.send(payMethods);
+app.get('/available_payments', (_req, res) => {
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  });
 });
 
-app.post("/login", (req, resp) => {
-    const userName = request.body.userName;
-    if (userName) {
-	resp.send(`Welcome ${userName}`);
-    } else {
-	resp.status(404).send();
-    }
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  res.send(`Welcome ${userName}`);
 });
 
 app.listen(port, () => {
-    console.log("API available on localhost port 7865");
+  console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
