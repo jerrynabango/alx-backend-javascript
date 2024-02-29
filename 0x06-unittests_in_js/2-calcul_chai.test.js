a0da0514208a3f4605018647caadfc03d61390f9
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-29 16:40:31 +0300

    chai assertion

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,35 +1,36 @@
-const { it, describe } = require("mocha");
-const calculateNumber = require("./1-calcul");
-const { expect } = require("chai");
+const expect = require("chai").expect;
+const {describe, it} = require("mocha");
+const calculateNumber = require("./2-calcul_chai");
 
-describe("calculateNumber()", function() {
-    it(`verifies the sum of rounded numbers`, function() {
-      const output = calculateNumber("SUM", 1, 2);
-      expect(output).to.equal(3);
+describe("calculateNumber", function() {
+    describe("SUM", function() {
+	it("checking if numbers round", function() {
+	    expect(calculateNumber("SUM", 1, 2)).to.equal(3);
+	});
+	it("checking if numbers round", function() {
+	    expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
+	});
     });
-
-    it(`verifies the difference of rounded numbers`, function() {
-      const output = calculateNumber("SUBTRACT", 1.4, 2.2);
-      expect(output).to.equal(-1);
+    describe("SUBTRACT", function() {
+	it("checking if numbers round", function() {
+	    expect(calculateNumber("SUBTRACT", 1.4, 2.3)).to.equal(-1);
+	});
+	it("checking if numbers round", function() {
+	    expect(calculateNumber("SUBTRACT", 4.9, 2.7)).to.equal(2);
+	});
+	it("checking if numbers round", function() {
+	    expect(calculateNumber("SUBTRACT", -4.9, -2.7)).to.equal(-2);
+	});
     });
-
-    it(`verifies the difference of rounded numbers`, function() {
-      const output = calculateNumber("SUBTRACT", 4.9, 2.7);
-      expect(output).to.equal(2);
-    });
-
-    it(`verifies the sum of rounded numbers`, function() {
-      const output = calculateNumber("DIVIDE", 4, 2);
-      expect(output).to.equal(2);
-    });
-
-    it(`verifies the division by zero error`, function() {
-      const output = calculateNumber("DIVIDE", 1.7, 0);
-      expect(output).to.equal("Error");
-    });
-
-    it(`verifies the quotient of rounded numbers`, function() {
-      const output = calculateNumber("DIVIDE", 1.4, 4.6);
-      expect(output).to.equal(0.2);
+    describe("DIVIDE", function() {
+	it("checking if numbers round", function() {
+	    expect(calculateNumber("DIVIDE", 4, 2)).to.equal(2);
+	});
+	it("checking if numbers round", function() {
+	    expect(calculateNumber("DIVIDE", 4.6, 1.8)).to.equal(2.5);
+	});
+	it("checking if numbers round", function() {
+	    expect(calculateNumber("DIVIDE", 4, 0)).to.equal("Error");
+	});
     });
 });
