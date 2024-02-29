aed77439e24ebd6f0974bcb82a23fdff98aa8832
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-29 16:51:41 +0300

    chai assertion

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,35 +1,38 @@
-const expect = require("chai");
-const { it, describe } = require("mocha");
+const chaiExpect = require("chai").expect;
+const { describe, it } = require("mocha");
 const calculateNumber = require("./2-calcul_chai");
 
-describe("calculateNumber()", function() {
-    it(`verifies the sum of rounded numbers`, function() {
-      const output = calculateNumber("SUM", 1, 2);
-      expect(output).to.equal(3);
+describe("calculateNumber", function() {
+    describe("SUM operation", function() {
+        it("verifies the sum of rounded numbers is correct", function() {
+            chaiExpect(calculateNumber("SUM", 1, 2)).to.equal(3);
+        });
+        it("verifies the sum of rounded decimal numbers is correct", function() {
+            chaiExpect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
+        });
     });
 
-    it(`verifies the difference of rounded numbers`, function() {
-      const output = calculateNumber("SUBTRACT", 1.4, 2.2);
-      expect(output).to.equal(-1);
+    describe("SUBTRACT operation", function() {
+        it("verifies the difference of rounded numbers is correct", function() {
+            chaiExpect(calculateNumber("SUBTRACT", 1.4, 2.3)).to.equal(-1);
+        });
+        it("verifies the difference of rounded numbers is correct", function() {
+            chaiExpect(calculateNumber("SUBTRACT", 4.9, 2.7)).to.equal(2);
+        });
+        it("verifies the difference of negative rounded numbers is correct", function() {
+            chaiExpect(calculateNumber("SUBTRACT", -4.9, -2.7)).to.equal(-2);
+        });
     });
 
-    it(`verifies the difference of rounded numbers`, function() {
-      const output = calculateNumber("SUBTRACT", 4.9, 2.7);
-      expect(output).to.equal(2);
-    });
-
-    it(`verifies the sum of rounded numbers`, function() {
-      const output = calculateNumber("DIVIDE", 4, 2);
-      expect(output).to.equal(2);
-    });
-
-    it(`verifies the division by zero error`, function() {
-      const output = calculateNumber("DIVIDE", 1.7, 0);
-      expect(output).to.equal("Error");
-    });
-
-    it(`verifies the quotient of rounded numbers`, function() {
-      const output = calculateNumber("DIVIDE", 1.4, 4.6);
-      expect(output).to.equal(0.2);
+    describe("DIVIDE operation", function() {
+        it("verifies the division of rounded numbers is correct", function() {
+            chaiExpect(calculateNumber("DIVIDE", 4, 2)).to.equal(2);
+        });
+        it("verifies the division of rounded decimal numbers is correct", function() {
+            chaiExpect(calculateNumber("DIVIDE", 4.6, 1.8)).to.equal(2.5);
+        });
+        it("verifies division by zero results in 'Error'", function() {
+            chaiExpect(calculateNumber("DIVIDE", 4, 0)).to.equal("Error");
+        });
     });
 });
