902133c71e17ae333fd600f1ecb26dfa78060c0f
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-29 16:16:45 +0300

    combining descriptions

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -3,12 +3,12 @@ function calculateNumber(type, a, b) {
     if (type === "SUM") {
       output = Math.round(a) + Math.round(b);
     } else if (type === "SUBTRACT") {
-      result = Math.round(a) - Math.round(b);
+      output = Math.round(a) - Math.round(b);
     } else if (type === "DIVIDE") {
       if (Math.abs(Math.round(b)) === 0) {
         output = "Error";
       } else {
-        result = Math.round(a) / Math.round(b);
+        output = Math.round(a) / Math.round(b);
       }
     }
     return output;
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,24 +2,34 @@ const assert = require("assert");
 const { it, describe } = require("mocha");
 const calculateNumber = require("./1-calcul");
 
-describe('calculateNumber()', function() {
-  it('sum of rounded numbers', function() {
-    const result = calculateNumber('SUM', 1.5, 2.7);
-    assert.strictEqual(result, 4);
-  });
+describe("calculateNumber()", function() {
+    it(`verifies the sum of rounded numbers`, function() {
+      const output = calculateNumber("SUM", 1, 2);
+      assert.strictEqual(output, 3);
+    });
 
-  it('difference of rounded numbers', function() {
-    const result = calculateNumber('SUBTRACT', 3.8, 1.2);
-    assert.strictEqual(result, 3);
-  });
+    it(`verifies the difference of rounded numbers`, function() {
+      const output = calculateNumber("SUBTRACT", 1.4, 2.2);
+      assert.strictEqual(output, -1);
+    });
 
-  it('division by zero error', function() {
-    const result = calculateNumber('DIVIDE', 5, 0);
-    assert.strictEqual(result, 'Error');
-  });
+    it(`verifies the difference of rounded numbers`, function() {
+      const output = calculateNumber("SUBTRACT", 4.9, 2.7);
+      assert.strictEqual(output, 2);
+    });
 
-  it('quotient of rounded numbers', function() {
-    const result = calculateNumber('DIVIDE', 10, 3);
-    assert.strictEqual(result, 3);
-  });
+    it(`verifies the sum of rounded numbers`, function() {
+      const output = calculateNumber("DIVIDE", 4, 2);
+      assert.strictEqual(output, 2);
+    });
+
+    it(`verifies the division by zero error`, function() {
+      const output = calculateNumber("DIVIDE", 1.7, 0);
+      assert.strictEqual(output, "Error");
+    });
+
+    it(`verifies the quotient of rounded numbers`, function() {
+      const output = calculateNumber("DIVIDE", 1.4, 4.6);
+      assert.strictEqual(output, 0.2);
+    });
 });
