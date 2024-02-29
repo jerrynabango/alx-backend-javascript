7cf69ea23e8a2e8f448566eb28d09087e603fdee
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-29 15:43:26 +0300

    basic Mocha test

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,31 @@
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./0-calcul");
+
+describe("validateRoundingBehavior()", function() {
+
+    it(`should round 1 and 2 to 3`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
+    });
+    it(`should round 1.4 and 2.2 to 3`, function() {
+      const res = calculateNumber(1.4, 2.2);
+      assert.strictEqual(res, 3);
+    });
+    it(`should round 1.6 and 2.7 to 5`, function() {
+      const res = calculateNumber(1.6, 2.7);
+      assert.strictEqual(res, 5);
+    });
+    it(`should round 0 and 0 to 0`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
+    });
+    it(`should round -1.6 and -1.7 to -4`, function() {
+      const res = calculateNumber(-1.6, -1.7);
+      assert.strictEqual(res, -4);
+    });
+    it(`should round -1.4 and -1.3 to -2`, function() {
+      const res = calculateNumber(-1.4, -1.3);
+      assert.strictEqual(res, -2);
+    });
+});
