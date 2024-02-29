c96d054bd3adbc52d869413df2c5b6446bfc5a80
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-29 16:06:39 +0300

    combining descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,24 +2,24 @@ const assert = require("assert");
 const { it, describe } = require("mocha");
 const calculateNumber = require("./1-calcul");
 
-describe("calculateNumber()", function() {
-  it("sum of rounded numbers", function() {
-    const result = calculateNumber("SUM", 1.5, 2.7);
+describe('calculateNumber()', function() {
+  it('sum of rounded numbers', function() {
+    const result = calculateNumber('SUM', 1.5, 2.7);
     assert.strictEqual(result, 4);
   });
 
-  it("difference of rounded numbers", function() {
-    const result = calculateNumber("SUBTRACT", 3.8, 1.2);
+  it('difference of rounded numbers', function() {
+    const result = calculateNumber('SUBTRACT', 3.8, 1.2);
     assert.strictEqual(result, 3);
   });
 
-  it("division by zero error", function() {
-    const result = calculateNumber("DIVIDE", 5, 0);
-    assert.strictEqual(result, "Error");
+  it('division by zero error', function() {
+    const result = calculateNumber('DIVIDE', 5, 0);
+    assert.strictEqual(result, 'Error');
   });
 
-  it("quotient of rounded numbers", function() {
-    const result = calculateNumber("DIVIDE", 10, 3);
+  it('quotient of rounded numbers', function() {
+    const result = calculateNumber('DIVIDE', 10, 3);
     assert.strictEqual(result, 3);
   });
 });
