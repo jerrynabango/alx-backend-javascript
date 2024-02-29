4c6a6da75f8da41ead0eeed69f1430e163d4420a
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-29 22:41:42 +0300

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -5,7 +5,7 @@ const { expect } = chai;
 describe("Index page", () => {
   it("should respond with welcome message", (done) => {
     try {
-      req("http://localhost:7865/", (error, resp, body) => {
+      request("http://localhost:7865/", (error, resp, body) => {
         if (error) throw error;
         expect(body).to.equal("Welcome to the payment system");
         expect(resp.statusCode).to.equal(200);
@@ -18,7 +18,7 @@ describe("Index page", () => {
   describe("Cart endpoint", () => {
     it("should respond with payment methods for the specified cart", (done) => {
       try {
-        req("http://localhost:7865/cart/12", (error, resp, body) => {
+        request("http://localhost:7865/cart/12", (error, resp, body) => {
           if (error) throw error;
           expect(body).to.equal("Payment methods for cart 12");
           expect(resp.statusCode).to.equal(200);
@@ -30,7 +30,7 @@ describe("Index page", () => {
     });
     it("should respond with status 404 if cart ID is not a number", (done) => {
       try {
-        req("http://localhost:7865/cart/uber", (error, resp) => {
+        request("http://localhost:7865/cart/uber", (error, resp) => {
           if (error) throw error;
           expect(resp.statusCode).to.equal(404);
           done();
@@ -43,7 +43,7 @@ describe("Index page", () => {
   describe("Available payments endpoint", () => {
     it("should respond with available payment methods", (done) => {
       try {
-        req("http://localhost:7865/available_payments", (error, resp, body) => {
+        request("http://localhost:7865/available_payments", (error, resp, body) => {
           if (error) throw error;
           expect(JSON.parse(body)).to.deep.equal({
             payment_methods: {
@@ -62,7 +62,7 @@ describe("Index page", () => {
   describe("Login endpoint", () => {
     it("should respond with a welcome message for the specified user", (done) => {
       try {
-        req.post({
+        request.post({
           url: "http://localhost:7865/login",
           json: { userName: "Betty" },
         }, (error, resp, body) => {
