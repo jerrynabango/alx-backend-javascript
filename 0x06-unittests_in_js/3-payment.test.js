17607168a76c5c4036d9ce8816f77f6abcef72ad
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-29 23:17:29 +0300

    use of spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,27 @@
-const assert = require("assert");
 const Utils = require("./utils");
+const chai = require("chai");
 const sinon = require("sinon");
-const {describe, it} = require("mocha");
+const expect = chai.expect;
 const sendPaymentRequestToApi = require("./3-payment");
 
-describe("sendPaymentRequestToApi", function() {
-    it("check that Utils.calculateNumber was called once", function() {
-	const spy = sinon.spy(Utils, "calculateNumber");
-
-	sendPaymentRequestToApi(50, 24.52);
-
-	assert(spy.calledOnce);
-	spy.restore();
-    });
+describe("sendPaymentRequestToAp", function() {
+  beforeEach(function() {
+    sinon.spy(Utils, "calculateNumber");
+  });
+  afterEach(function() {
+    sinon.restore();
+  });
+  it("validate the usage of the `Utils.calculateNumber` function", function() {
+    sendPaymentRequestToApi(100, 20);
+    // Check it is called
+    expect(Utils.calculateNumber.called).to.be.true;
+    // Check it is called once
+    expect(Utils.calculateNumber.calledOnce).to.be.true;
+    // Check call count
+    expect(Utils.calculateNumber.callCount).to.be.equal(1);
+    // Check it first argument
+    expect(Utils.calculateNumber.firstCall.args[0]).to.equal("SUM");
+    // Check params
+    expect(Utils.calculateNumber.calledWith("SUM", 100, 20)).to.be.true;
+  });
 });
